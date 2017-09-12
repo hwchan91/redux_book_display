2d2a80f5c032e6ecfc0998bec018e743e2909a5f
hwchan91 <hwchan91@gmail.com>
2017-09-12 18:51:51 +0800

    complete

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Harry Potter', pages: 500},
+        {title: 'Tale of Two Cities', pages: 888},
+        {title: 'Metamorphosis', pages: 1012},
+        {title: 'Hamlet', pages: 50}
+    ]
+}
